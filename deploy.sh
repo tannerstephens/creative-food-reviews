cd153cf92a08b9bf770c5305a23bb5b6dc8ece5f
Tanner Stephens <tannerrstephens@gmail.com>
2020-01-10 01:45:30 -0600

    Fix Deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@ PIPENV_VENV_IN_PROJECT=1 /usr/local/bin/pipenv install
 PIPENV_VENV_IN_PROJECT=1 FLASK_APP="cfr:create_app()" /usr/local/bin/pipenv run flask db init
 PIPENV_VENV_IN_PROJECT=1 FLASK_APP="cfr:create_app()" /usr/local/bin/pipenv run flask db upgrade
 
-sudo service ghosted restart
+sudo service cfr restart
